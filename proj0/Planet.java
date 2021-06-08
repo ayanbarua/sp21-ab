f8851568f2424965486d3f109b6d5a3a73c5f5dd
Ayan <aybarua@gmail.com>
2021-06-07 23:03:37 -0500

    added calcDistance and calcForceExertedBy methods

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,6 +5,7 @@ public class Planet {
 	public double yyVel;
 	public double mass;
 	public String imgFileName;
+	private static final double G = 6.67e-11; 
 
 	public Planet(double xP, double yP, double xV, double yV, double m, String img) {
 		xxPos = xP;
@@ -24,5 +25,18 @@ public class Planet {
 		mass = p.mass;
 		imgFileName = p.imgFileName;
 	}
+
+	public double calcDistance(Planet rocinate) {
+		double dx = this.xxPos - rocinate.xxPos;
+		double dy = this.yyPos - rocinate.yyPos;
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+
+	public double calcForceExertedBy(Planet rocinate) {
+		double force = (G * this.mass * rocinate.mass) / (this.calcDistance(rocinate) * this.calcDistance(rocinate));
+		return force;
+	}
+
+
 
 }
